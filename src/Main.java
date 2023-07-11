3dea9a2686990db5f7c1a71e6da2a5de0a3b3f06
ValeryBurlakov <wogl@mail.ru>
2023-07-11 22:26:20 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main { //
+    public static void main(String[] args) { //
+        LinkedList list = new LinkedList(); //
+
+        list.add(5); //
+        list.add(2); //
+        list.add(9); //
+        list.add(0); //
+        list.add(-4); //
+
+//        boolean flag = list.remove(12); // проверка на наличие ноды в списке
+//        list.remove(-4);
+//        list.removeAt(2);
+//        System.out.println(list.getValue(4));
+//        list.addAt(-1, 5);
+        list.print(); //
+//        list.quickSort();
+        list.reverseList();
+        list.print(); //
+//        System.out.println(flag);
+    }
+    public static void quickSort(int[] source, int leftBorder, int rightBorder) {
+
+    }
+}
